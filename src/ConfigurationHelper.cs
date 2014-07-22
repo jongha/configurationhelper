3892960c51fa192ebb300f2e22e2dc485741c417
Jong-Ha Ahn <jongha.ahn@mrlatte.net>
2014-07-22 15:34:52 +0900

    init project

---
+++ src/ConfigurationHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace ConfigurationHelper
+{
+    public static class ConfigurationHelper
+    {
+        public static NameValueCollection AppSettings
+        {
+            get
+            {
+                return ConfigurationManager.AppSettings;
+            }
+        }
+
+        public static ExtendedNameValueCollection<int> AppIntSettings
+        {
+            get
+            {
+                return (ExtendedNameValueCollection<int>)ConfigurationManager.AppSettings;
+            }
+        }
+
+        public static ConnectionStringSettingsCollection ConnectionStrings
+        {
+            get
+            {
+                return ConfigurationManager.ConnectionStrings;
+            }
+        }
+
+        public static object GetSection(string sectionName)
+        {
+            return ConfigurationHelper.GetSection(sectionName);
+        }
+
+        public static Configuration OpenExeConfiguration(ConfigurationUserLevel userLevel)
+        {
+            return ConfigurationHelper.OpenExeConfiguration(userLevel);
+        }
+
+        public static Configuration OpenExeConfiguration(string exePath)
+        {
+            return ConfigurationHelper.OpenExeConfiguration(exePath);
+        }
+
+        public static Configuration OpenMachineConfiguration()
+        {
+            return ConfigurationHelper.OpenMachineConfiguration();
+        }
+
+        public static Configuration OpenMappedExeConfiguration(ExeConfigurationFileMap fileMap, ConfigurationUserLevel userLevel)
+        {
+            return ConfigurationHelper.OpenMappedExeConfiguration(fileMap, userLevel);
+        }
+
+        public static Configuration OpenMappedMachineConfiguration(ConfigurationFileMap fileMap)
+        {
+            return ConfigurationHelper.OpenMappedMachineConfiguration(fileMap);
+        }
+
+        public static void RefreshSection(string sectionName)
+        {
+            ConfigurationHelper.RefreshSection(sectionName);
+        }
+    }
+}
---
+++ src/ExtendedNameValueCollection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Specialized;
+using System.Reflection;
+
+namespace ConfigurationHelper
+{
+    public class ExtendedNameValueCollection<T>
+    {
+        private NameValueCollection collection = null;
+
+        public static explicit operator ExtendedNameValueCollection<T>(NameValueCollection collection)
+        {
+            ExtendedNameValueCollection<T> extendedCollection = new ExtendedNameValueCollection<T>();
+            extendedCollection.collection = collection;
+
+            return extendedCollection;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return (T)typeof(T).GetMethod("Parse", new[] { typeof(T) }).Invoke(null, new Object[] { collection[index] });
+            }
+        }
+
+        public T this[string name]
+        {
+            get
+            {
+                return (T)typeof(T).GetMethod(
+                    "Parse", 
+                    new Type[] { typeof(string) }
+                    ).Invoke(null, new Object[] { collection[name] });
+            }
+
+            set
+            {
+                collection[name] = value.ToString();
+            }
+        }
+    }
+}
